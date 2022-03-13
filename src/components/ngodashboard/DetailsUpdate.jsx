ec5625a6cf82e3f7493e9c4b09a4eb21be522262
testKeder <keder.nath.mallick.2019@cse.tict.edu.in>
2022-03-13 21:32:55 +0530

    local changed

--- src/components/ngodashboard/DetailsUpdate.jsx
+++ src/components/ngodashboard/DetailsUpdate.jsx
@@ -21,13 +21,9 @@ import List from '@mui/material/List';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import Collapse from '@mui/material/Collapse';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import DraftsIcon from '@mui/icons-material/Drafts';
-import SendIcon from '@mui/icons-material/Send';
+import Collapse from '@mui/material/Collapse'; 
 import ExpandLess from '@mui/icons-material/ExpandLess';
-import ExpandMore from '@mui/icons-material/ExpandMore';
-import StarBorder from '@mui/icons-material/StarBorder';
+import ExpandMore from '@mui/icons-material/ExpandMore'; 
 
 const data={ 
     id : "XXXXX/XXXX/XXXXX",
